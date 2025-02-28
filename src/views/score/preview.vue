9cc9663d01cc712ef57439e9700ab49698ab857f
Alever <759636538@qq.com>
2025-02-28 21:42:29 +0800

    [update] Update ErrorType styles.

--- src/views/score/preview.vue
+++ src/views/score/preview.vue
@@ -39,21 +39,122 @@
                 v-if="currentCourse && currentErrors"
                 class="description-block"
             >
-                <h3 class="title">
-                    感谢您的评测, 当前学科是: {{ currentCourse.name }}
+                <h2 class="title">
+                    感谢您参与我们的研究。当前学科是: {{ currentCourse.name }}
+                </h2>
+                <span>
+                    <p>
+                        在我们搭建的评测网站<a href="http://gaokao.fzuacm.com"
+                            >gaokao.fzuacm.com</a
+                        >中，您看到的【题目】均出自2010-2022年间的高考真题，【作答】均由<strong>人工智能大模型</strong>完成。
+                    </p>
+
+                    <blockquote>
+                        <p>
+                            在这些“答案”中，有一些是由AI模型扮演“考生”正常作答，更多的则是由AI模型扮演“差生”在已知参考答案的前提下，按照常见错因故意生成的错误答案。
+                        </p>
+                    </blockquote>
+
+                    <p>
+                        您将<strong>扮演“高考阅卷教师”</strong>的角色，按照中学考试阅卷中的正常流程，依照每道题给出的参考答案，为这些“AI考生”的作答给出评分。
+                    </p>
+
+                    <h3 style="text-align: start">具体操作内容如下：</h3>
+                    <ol style="padding: 0px 15px; box-sizing: border-box">
+                        <li>
+                            <p>
+                                <strong
+                                    style="
+                                        text-decoration: none;
+                                        font-weight: 600;
+                                    "
+                                    >选择学科模块: </strong
+                                >您需要在最上方的<strong
+                                    style="
+                                        text-decoration: none;
+                                        font-weight: 600;
+                                    "
+                                    ><span style="color: #958df1"
+                                        >导航栏</span
+                                    ></strong
+                                >选择自己<strong>学科对应的评分模块</strong>。
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                <strong
+                                    style="
+                                        text-decoration: none;
+                                        font-weight: 600;
+                                    "
+                                    >评总分和步骤分: </strong
+                                >对于每道题，我们给出了参考答案和该题总分，并<strong>将学生作答划分为了若干个步骤</strong>。您需要对<strong>划分后的步骤</strong>进行评分，如该步骤有错误，您需要<strong>在评步骤分的同时，选择我们预设好的“错误原因”</strong>。
+                            </p>
+                            <ul style="padding: 0px 15px">
+                                <li>
+                                    <p>
+                                        程序是按照“换行Enter”进行步骤的自动切分，所以这里的“步骤1”、“步骤2”……可能并不代表实际解题中的步骤。
+                                    </p>
+                                </li>
+                                <li>
+                                    <p>
+                                        请注意！<strong
+                                            >所有的“步骤分”都指的是该步骤的“区间得分”，而非截止到当前步骤的“累计分”！</strong
+                                        >
+                                    </p>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <p>
+                                <strong
+                                    style="
+                                        text-decoration: none;
+                                        font-weight: 600;
+                                    "
+                                    >计算总分: </strong
+                                >评完所有步骤分后，可以点击<strong>“累计步骤分”</strong>，系统将自动计算该题总分。您也可以进一步修改总分。该题评分完成后，该题的标签会由<mark
+                                    data-color="#ffa8a8"
+                                    style="
+                                        background-color: #ffa8a8;
+                                        color: inherit;
+                                    "
+                                    >红色</mark
+                                >变为<mark
+                                    data-color="#8ce99a"
+                                    style="
+                                        background-color: #8ce99a;
+                                        color: inherit;
+                                    "
+                                    >绿色</mark
+                                >。
+                            </p>
+                        </li>
+                    </ol>
+                    <h5>
+                        您可以在导航栏下方看到自己的评测进度。可以选择切换【全部作答】、【已评分】、【未评分】。
+                    </h5>
+                </span>
+                <h3 class="split-title" style="margin: 5px 0px">
+                    该学科包含以下常见错误类型:
                 </h3>
-                <p class="split-title">该学科包含以下常见错误类型:</p>
-                <div v-for="(error, eidx) in currentErrors.errors" :key="eidx">
-                    <h4
-                        style="
-                            padding-left: 5px;
-                            border-left: 3px rgba(220, 219, 239, 0.8) solid;
-                            color: rgba(180, 159, 233, 1);
-                        "
-                    >
+                <div
+                    v-for="(error, eidx) in currentErrors.errors"
+                    :key="eidx"
+                    style="
+                        width: auto;
+                        padding: 15px;
+                        margin: 10px 0px;
+                        background: rgba(180, 159, 233, 0.3);
+                        color: rgba(50, 49, 48, 1);
+                        border-radius: 6px;
+                        box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.1);
+                    "
+                >
+                    <h3>
                         {{ error.name }}
-                    </h4>
-                    <p style="font-size: 13px">{{ error.description }}</p>
+                    </h3>
+                    <p style="font-size: 13px;">{{ error.description }}</p>
                 </div>
             </div>
             <div
@@ -75,7 +176,9 @@
                         >
                             No.{{ (page - 1) * page_num + index + 1 }}:
                         </p>
-                        <p class="question-title" style="margin-left: 5px;">题目标识ID: {{ item.id }}</p>
+                        <p class="question-title" style="margin-left: 5px">
+                            题目标识ID: {{ item.id }}
+                        </p>
                     </div>
                     <p class="score-title">分值: {{ item.score }}</p>
                 </div>
@@ -590,6 +693,11 @@ export default {
             background: rgba(200, 200, 200, 0.1);
             border-radius: 12px;
             line-height: 2;
+
+            strong {
+                font-weight: 400;
+                text-decoration: underline;
+            }
         }
 
         .sample-item {
